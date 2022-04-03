9b7661554dcbd790654d0ef416987342f5cd2e94
KANAK MALVIYA <60460750+Kanakmalviya04@users.noreply.github.com>
2022-04-03 14:13:13 +0530

    Assignment 13/14 Added

---
+++ April/Week 1/Assignment 13+14/Program_2.js
@@ -0,0 +1,22 @@
+
+
+// 2. Write a program to have 3 variables (all three are number) and output the highest/greatest/maximum of all three.
+
+function find(){ 
+
+    var a = 40;
+    var b = 50;
+    var c = 300;
+
+        if(a > b && a > c){
+
+            console.log('A is Greater i.e. =', a);
+        }
+        else if(b > c){
+
+            console.log('B is Greater i.e. =', b);
+        }
+         else{
+        console.log('C is Greater i.e. =', c);
+         }
+}
