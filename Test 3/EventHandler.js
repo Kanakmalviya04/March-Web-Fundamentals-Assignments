dffe3ac01e8660d46e888814e0837fb6016135df
KANAK MALVIYA <60460750+Kanakmalviya04@users.noreply.github.com>
2022-04-10 18:17:26 +0530

    Test of 10th April Added

---
+++ Test 3/EventHandler.js
@@ -0,0 +1,9 @@
+
+
+// Make an On-event handler in which you have to make an input box and a submit button.
+// After clicking on submit button the input you gave should be displayed in an alert. Give The hosted Link here.
+
+var pressedButton = document.getElementsByTagName("button")[0];
+pressedButton.addEventListener("click", function (event) {
+   alert("Are You Confirm To INN.........")
+})
---
+++ Test 3/Swapping.js
@@ -0,0 +1,11 @@
+
+
+//JavaScript Program to swap two numbers with using third variable.
+var a = prompt('Enter Any 1st Value')
+var b = prompt('Enter Any 2nd Value')
+var c = a
+var a = b
+var b = c
+
+console.log('Value of a is :-', a)
+console.log('Value of b is :-', b)
